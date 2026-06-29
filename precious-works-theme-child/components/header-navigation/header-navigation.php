de7ac9e306a2e9365775895f495d80a09aab7d27
bijonl <bijon2591@gmail.com>
2026-06-29 16:49:28 -0400

    new phone number

--- precious-works-theme-child/components/header-navigation/header-navigation.php
+++ precious-works-theme-child/components/header-navigation/header-navigation.php
@@ -12,6 +12,13 @@ $header_phone_number = get_field('header_phone_number', $id);
 <header id="site-header" class="site-header">
   <div class="site-header-container container">
     <div class="site-header-row row align-items-center">
+    <!-- Mobile Phone -->
+      <div class="header-menu-col-phone d-block d-xl-none text-end">
+          <a href="tel:<?php echo preg_replace('/[^0-9+]/', '', $header_phone_number ? $header_phone_number : $header_nav_phone_number); ?>"
+            aria-label="Call us at <?php echo esc_attr($header_phone_number ? $header_phone_number : $header_nav_phone_number); ?>">
+              <?php echo esc_html($header_phone_number ? $header_phone_number : $header_nav_phone_number); ?>
+          </a>
+      </div>
       <!-- Shared Logo -->
       <div class="site-header-logo-col col">
         <div class="site-brand-logo-wrapper d-flex">
@@ -19,10 +26,10 @@ $header_phone_number = get_field('header_phone_number', $id);
         </div>
       </div>
       <!-- Desktop Menu -->
-      <div class="header-menu-col col-sm-7 d-none d-lg-block">
+      <div class="header-menu-col col-sm-7 col-lg-8 col-xl-7 d-none d-lg-block">
         <?php include(locate_template('components/header-navigation/partials/header-menu.php')); ?>
       </div>
-      <div class="header-menu-col-phone col-sm-3">
+      <div class="header-menu-col-phone d-none d-xl-block col-sm-3">
           <a href="tel:<?php echo preg_replace('/[^0-9+]/', '', $header_phone_number ? $header_phone_number : $header_nav_phone_number); ?>"
             aria-label="Call us at <?php echo esc_attr($header_phone_number ? $header_phone_number : $header_nav_phone_number); ?>">
               <?php echo esc_html($header_phone_number ? $header_phone_number : $header_nav_phone_number); ?>
